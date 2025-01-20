95510783912fbc0eafc1713b2e1283b8037f4cc7
arinaleech <ndjdn6062@gmail.com>
2025-01-20 21:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002250516135")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002367641884"))
 
 
 # Mongo DB Database Config
