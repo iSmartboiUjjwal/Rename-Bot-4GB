33242fe275af90ca5b08322123f61af0e3e2398a
arinaleech <ndjdn6062@gmail.com>
2025-01-20 21:33:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002250516135")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "bimbobot69")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002367641884"))
 
 
